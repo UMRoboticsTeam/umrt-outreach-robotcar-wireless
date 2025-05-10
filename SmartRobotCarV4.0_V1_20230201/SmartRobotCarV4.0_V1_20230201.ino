18408fcb9a73c2b1a40faaab7bc7ad748a1e0e8c
Backback12 <cepagtakhan@gmail.com>
2025-05-10 00:24:54 -0500

    Clean unused smartrobotcar reference file

--- SmartRobotCarV4.0_V1_20230201/SmartRobotCarV4.0_V1_20230201.ino
+++ SmartRobotCarV4.0_V1_20230201/SmartRobotCarV4.0_V1_20230201.ino
@@ -10,15 +10,15 @@
 #include "ApplicationFunctionSet_xxx0.h"
 
 
-#include <Adafruit_SSD1306.h>
+// #include <Adafruit_SSD1306.h>
 // #include "FluxGarage_RoboEyes.h"
 // roboEyes roboEyes;
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
+// #define SCREEN_WIDTH 128
+// #define SCREEN_HEIGHT 64
+// #define OLED_RESET -1
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 
@@ -30,14 +30,14 @@ void setup()
 
 
   // start display
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.print("YOOO IS THIS WORKING???");
-  display.display();
+  // if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  //   Serial.println(F("SSD1306 allocation failed"));
+  //   for(;;);
+  // }
+  // display.clearDisplay();
+  // display.setCursor(0, 0);
+  // display.print("YOOO IS THIS WORKING???");
+  // display.display();
 
   // setup robo eyes
   // roboEyes.begin(S)
